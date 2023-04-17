cc44ac225dd9e574b822d86fd722cbb843a3120b
Qwaieare <gb-grand@mail.ru>
2023-04-17 23:32:31 +0300

    initial commit for hw20230417_withMockito

---
+++ src/main/java/skypro/withmockito/Hw20230417WithMockitoApplication.java
@@ -0,0 +1,13 @@
+package skypro.withmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw20230417WithMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw20230417WithMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/withmockito/Hw20230417WithMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.withmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw20230417WithMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
